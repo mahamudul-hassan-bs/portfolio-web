72a2c8e46c95a4a104d1e0a1720dba9b8a3fc8f7
mahamudul-hassan-bs <mahamudul.hassan@brainstation-23.com>
2026-02-14 19:46:21 +0600

    allow all host debug

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,22 +22,14 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-//     credentials: true,
-//   }),
-// );
+Middleware
 app.use(
   cors({
-    origin: function (origin, callback) {
-      console.log("Incoming Origin:", origin);
-      callback(null, true); // allow request
-    },
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
     credentials: true,
-  })
+  }),
 );
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
